4de6a741b5247286d777ea0da4cfdd980dc03bb5
Teresa Nededog <tnededog@my.hpu.edu>
2015-01-20 17:07:22 -1000

    Create Arrays

--- build_arrays.js
+++ build_arrays.js
@@ -4,53 +4,68 @@
 
 // Create an empty array.
 
+  var emptyArray = [];
 
 // Create an array of the colors of the rainbow.
 
+  var colorsOfRainbow = ["red", "orange", "yellow", "green", "blue", "purple"];
 
 // Create an array of gems.
 
+  var arrayOfGems = ["granite", "saphhire", "onyx", "diamond", "ruby"];
 
 // Create an array of military ranks.
 
+  var arrayOfMilitaryRanks = ["PVT", "PV2", "PFC", "SPC", "CPL"];
 
 // Create an array of road signs.
-
+  
+  var arrayOfRoadSigns = ["School", "JCT", "Yield", "STOP"];
 
 // Create an array containing components of a tree.
 
+  var arrayOfTreeParts = ["Roots", "Trunk", "Branches", "Leaves", "Fruit"];
 
 // Create an array of prime numbers less than 20.
 
+  var arrayOfPrimeNumbers = [2, 3, 5, 7, 11, 13, 17, 19];
 
 // Create an array of words programmers typically say while programming.
 
+  var arrayOfProgrammerJargon = ["Jam", "Cool", "Push it!", "Oh my god"];
 
 // Create an array of elements of the periodic table.
 
+  var arrayOfElements = ["Hydrogen", "Oxygen", "Iron", "Lithium", "Calcium"];
 
 // Create an array representing the coins currently in your pocket.
 
+  var arrayOfCoins = ["Penny", "Nickel", "Dime", "Quarter"];
 
 // Create an array of items you bring to class daily.
 
+  var arrayOfClassItems = ["Laptop", "Snacks", "Thermos", "Jacket", "Charger"];
 
 // Create an array of web developer role titles.
 
+  var arrayOfDevTitles = ["Web Designer", "Front End Developer", "UI Designer", "UX Designer"];
 
 // Create an array of good luck charms.
 
-
+  var arrayOfLuckyCharms = ["Hearts", "Stars", "Horseshoes", "Clovers", "Blue Moons"];
 // Create an array of mythical animals.
 
+  var arrayOfMythCreatures = ["Nessy", "Big Foot", "Yetti", "Mermaid", "Nymph"];
 
 // Create an array of drinks.
 
+  var arrayOfDrinks = ["Whisky", "Tequila", "Vodka", "Rum", "Gin"];
 
 // Create an array of emoticons.
 
+  var  arrayOfEmoticons = ["Crying", "Angry", "Horror", "Frown"];
 
 // Create an array with the letters of the word 'array'.
 
-
+  var arrayOfLettersInArray = ["A", "R", "r", "a", "Y"];
 
